37125933eed583885b6c6b5afdcb83527fd1af92
Andrei Robu <andreidrobu@gmail.com>
2025-09-25 12:09:08 +0300

    fix recipe list change detection

--- webui/src/app/(app)/recipe/page.tsx
+++ webui/src/app/(app)/recipe/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useId } from "react";
 import { DndContext } from "@dnd-kit/core";
 import { SortableContext, useSortable } from "@dnd-kit/sortable";
 import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
@@ -13,6 +14,37 @@ import {
   Save,
 } from "lucide-react";
 import { useState } from "react";
+
+interface SongsBlock {
+  song_title: string;
+  artist_name: string;
+}
+
+interface PodcastBlock {
+  podcast_name: string;
+  backup_podcast_name: string;
+}
+
+interface RadioStationBlock {
+  station_name: string;
+}
+
+interface CaddyFileBlock {
+  url: string;
+  strategy: "file" | "cycle";
+}
+
+// Discriminated union for all recipe items
+type RecipeItem =
+  | ({ type: "songs" } & SongsBlock)
+  | ({ type: "podcast" } & PodcastBlock)
+  | ({ type: "radio" } & RadioStationBlock)
+  | ({ type: "caddy" } & CaddyFileBlock);
+
+type WithID<T> = {
+  id: number;
+  item: T;
+};
 
 function Sortable({ id }: { id: number }) {
   const { setNodeRef, attributes, listeners, transform, transition } =
@@ -47,20 +79,81 @@ const GRID_BUTTON_CLASSES =
   "flex justify-center gap-1.5 px-2 py-2 cursor-pointer rounded-sm";
 
 export default function Page() {
-  const [items, setItems] = useState([0, 1, 2, 3]);
+  const originalItems: WithID<RecipeItem>[] = [
+    {
+      id: 1,
+      item: {
+        type: "songs",
+        song_title: "Song Title",
+        artist_name: "Artist Name",
+      },
+    },
+    {
+      id: 2,
+      item: {
+        type: "podcast",
+        podcast_name: "Podcast Name",
+        backup_podcast_name: "Backup Podcast Name",
+      },
+    },
+    {
+      id: 3,
+      item: {
+        type: "radio",
+        station_name: "Station Name",
+      },
+    },
+    {
+      id: 4,
+      item: {
+        type: "caddy",
+        url: "https://example.com/file.txt",
+        strategy: "file",
+      },
+    },
+  ];
+
+  // const [seq, setSeq] = useState(100);
+  // const nextID = () => {
+  //   setSeq((s) => s + 1);
+  //   return seq;
+  // };
+
+  const [items, setItems] = useState(originalItems);
+  const [hasChanges, setHasChanges] = useState(false);
 
   const handleDragEnd = (event: any) => {
-    const { active, over } = event;
-    if (active.id !== over.id) {
+    const activeId = event.active.id;
+    const overId = event.over.id;
+
+    if (activeId !== overId) {
       setItems((items) => {
-        const oldIndex = items.indexOf(active.id);
-        const newIndex = items.indexOf(over.id);
+        const oldIndex = items.findIndex((i) => i.id === activeId);
+        const newIndex = items.findIndex((i) => i.id === overId);
         const newItems = [...items];
         newItems.splice(oldIndex, 1);
-        newItems.splice(newIndex, 0, active.id);
+        newItems.splice(newIndex, 0, items[oldIndex]);
+        // before checking for equality, keep only the id and item keys
+        const minimalOriginal = originalItems.map(({ id, item }) => ({
+          id,
+          item,
+        }));
+        const minimalNew = newItems.map(({ id, item }) => ({ id, item }));
+        setHasChanges(
+          !(JSON.stringify(minimalOriginal) === JSON.stringify(minimalNew))
+        );
         return newItems;
       });
     }
+  };
+
+  const handleCancel = () => {
+    setItems(originalItems);
+    setHasChanges(false);
+  };
+
+  const handleSave = () => {
+    console.log("Saved items:", items);
   };
 
   return (
@@ -70,12 +163,13 @@ export default function Page() {
       </p>
       <div className="flex flex-col gap-3 m-3">
         <DndContext
+          id={useId()}
           modifiers={[restrictToVerticalAxis]}
           onDragEnd={handleDragEnd}
         >
           <SortableContext items={items}>
-            {items.map((id) => (
-              <Sortable key={id} id={id} />
+            {items.map((element) => (
+              <Sortable key={element.id} id={element.id} />
             ))}
           </SortableContext>
         </DndContext>
@@ -102,7 +196,13 @@ export default function Page() {
         <div className="flex gap-3 m-3">
           <button
             type="button"
-            className={GRID_BUTTON_CLASSES + " flex-1 bg-black/35 text-white"}
+            className={
+              GRID_BUTTON_CLASSES +
+              " flex-1 bg-black/35 text-white disabled:opacity-50 " +
+              "disabled:cursor-not-allowed"
+            }
+            disabled={!hasChanges}
+            onClick={handleCancel}
           >
             <Eraser className="inline-block" />
             <span>Cancel Changes</span>
@@ -110,8 +210,12 @@ export default function Page() {
           <button
             type="button"
             className={
-              GRID_BUTTON_CLASSES + " flex-1 bg-salvation-green text-white"
+              GRID_BUTTON_CLASSES +
+              " flex-1 bg-salvation-green text-white disabled:opacity-50 " +
+              "disabled:cursor-not-allowed"
             }
+            disabled={!hasChanges}
+            onClick={handleSave}
           >
             <Save className="inline-block" />
             <span>Save Changes</span>
